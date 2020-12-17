0d2fa55d81e1c76dd947df91ac70f7137b11f084
Tory S. Anderson <torys.anderson@gmail.com>
2020-12-17 16:04:52 -0700

    content appears

--- content/content-page.php
+++ content/content-page.php
@@ -3,7 +3,7 @@
  *
  * The template for displaying pages
  *
- * Used in page.php and page tempaltes
+ * Used in page.php and page templates
  *
  * @package Septera
  */
@@ -26,11 +26,14 @@
 			<?php cryout_singular_before_inner_hook();  ?>
 
 			<div class="entry-content" <?php cryout_schema_microdata( 'text' ); ?>>
-				<?php the_content(); ?>
-				<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'septera' ), 'after' => '</div>' ) ); ?>
+			    <?php
+			    the_content();
+			    get_template_part('template-parts/flexible-content');
+			    ?>
+			    <?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'septera' ), 'after' => '</div>' ) ); ?>
 			</div><!-- .entry-content -->
 
-			<?php cryout_singular_after_inner_hook();  ?>
+			<?php /*cryout_singular_after_inner_hook(); */ ?>
 		</div><!-- .article-inner -->
 	</article><!-- #post-## -->
 	<?php comments_template( '/comments.php', true ); ?>

dd5b7ca060000a76baad791decaa9cb1bda6a323
Melissa Robles <BYU\meliss26@humwp-dev.byu.edu>
2021-04-23 15:15:22 -0600

    made code for setting num posts per page simpler by adding ||

--- functions.php
+++ functions.php
@@ -322,13 +322,10 @@ function set_posts_per_page( $query ) {
 
   global $wp_the_query;
 
-  if ( ( ! is_admin() ) && ( $query === $wp_the_query ) && ( $query->is_search() ) ) {
+  if ( ( ! is_admin() ) && ( $query === $wp_the_query ) 
+  && ( $query->is_search() || $query->is_archive()) ) {
     $query->set( 'posts_per_page', 40 );
   }
-  elseif ( ( ! is_admin() ) && ( $query === $wp_the_query ) && ( $query->is_archive() ) ) {
-    $query->set( 'posts_per_page', 50 );
-  }
-  // Etc..
 
   return $query;
 }

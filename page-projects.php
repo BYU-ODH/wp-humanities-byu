169e86b7a793ab4227403252fc283b3661903258
Kelly Woods <BYU\kwoods8@humwp-dev.byu.edu>
2021-11-16 10:46:14 -0700

    Created php file for the projects page

---
+++ page-projects.php
@@ -0,0 +1,6 @@
+<div class="odhBoxContentGrid">
+	<!--main card content starts here-->
+	<div class="odhBoxCards">
+        <?php echo pods( 'projects', array( 'limit' => -1 ) )->template( 'Project Card Template' ); ?>
+ 	</div>
+</div>

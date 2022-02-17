f822ac008d77acdeb866548380d8136e1dfb5f05
MelRobles <melissa2016jatc@gmail.com>
2022-02-17 12:15:28 -0700

    saving style changes to blog posts and front page edites on blog

--- front-page.php
+++ front-page.php
@@ -35,45 +35,49 @@ if ( 'posts' == get_option( 'show_on_front' ) ) {
 	</div><!-- #container -->
 
 <!--Add posts to home page-->
-<!--Post container, sets space for the posts to display-->
-<div class="homePostContainer">
-	<!--Inner container to align post content with flex-->
-	<div class="homePostInner">
-		<ul>
-		<?php 
-		// WP Query Parameters to get blog posts
-		$the_query = new WP_Query( array('post_type' => 'post', 'posts_per_page' => 5) ); ?>
-		
-		<?php // Start of WP Query
-		while ($the_query -> have_posts()) : $the_query -> the_post(); 
-		// Display the Post Title with Hyperlink
-		?>
-		
-		<div class="homePostSingle">
-			<!--gets the post id which will help get the thumbnail/image-->
-			<?php $post_id = get_the_ID();?>
-			<div class="homePostIMG"><?php echo get_the_post_thumbnail( $post_id, 'thumbnail', array( 'class' => 'object-fit_cover' ) );?></div>
-			<div class="homePostText">
-				<li><a class="homePagePost" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-				</li>
-				<li><p class="homePostAuthor"><?php echo get_the_author(); ?></p></li>
-				<li><?php 
-				// Display the Post Excerpt
-				the_excerpt(__('(more…)')); ?></li>
-				<li><p class="homePostAuthor"><?php echo get_the_date( 'F j, Y', $post_id )?></p></li>
-			</div>
+<section class="blog">
+	<h1>Blog</h1>
+	<!--Post container, sets space for the posts to display-->
+	<div class="homePostContainer">
+		<!--Inner container to align post content with flex-->
+		<div class="homePostInner">
+			<ul>
+			<?php 
+			// WP Query Parameters to get blog posts
+			$the_query = new WP_Query( array('post_type' => 'post', 'posts_per_page' => 5) ); ?>
+			
+			<?php // Start of WP Query
+			while ($the_query -> have_posts()) : $the_query -> the_post(); 
+			// Display the Post Title with Hyperlink
+			?>
+			
+			<article class="homePostSingle">
+				<!--gets the post id which will help get the thumbnail/image-->
+				<?php $post_id = get_the_ID();?>
+				<div class="homePostIMG"><?php echo get_the_post_thumbnail( $post_id, 'thumbnail', array( 'class' => 'object-fit_cover' ) );?></div>
+				<div class="homePostText">
+					<li><a class="homePagePost" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+					</li>
+					<li><span class="homePostDate"><?php echo get_the_date( 'F j, Y', $post_id )?></span>
+						<span class="homePostAuthor"><?php echo get_the_author(); ?></span>
+					</li>
+					<li><?php 
+					// Display the Post Excerpt
+					the_excerpt(__('(more…)')); ?></li>
+				</div>
+			</article>
+			<hr class="homePostLine">
+			
+			<?php 
+			// Repeat the process and reset once it hits the limit
+			endwhile;
+			wp_reset_postdata();
+			?>
+			</ul>	
+			
 		</div>
-		<hr class="homePostLine">
-		
-		<?php 
-		// Repeat the process and reset once it hits the limit
-		endwhile;
-		wp_reset_postdata();
-		?>
-		</ul>	
-		
 	</div>
-</div>
+</section>
 <!--end of post content-->
 
 	<?php get_footer();

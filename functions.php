60882daaf06fbf213e1ebee0a53de018e4034bed
Kelly Woods <BYU\kwoods8@humwp-dev.byu.edu>
2022-03-07 10:25:30 -0700

    Added text editor and author to custom blog posts

--- functions.php
+++ functions.php
@@ -225,7 +225,7 @@ function byuh_setup() {
         'has_archive'         => false, 
         'hierarchical'        => false,
         'menu_position'       => 20,
-        'supports'            => array('title', 'thumbnail', 'excerpt'),
+        'supports'            => array('title', 'thumbnail', 'excerpt', 'editor', 'author'),
         'menu_icon'           => 'dashicons-feedback',
 
         // For the REST API v2

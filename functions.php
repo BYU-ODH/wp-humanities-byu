e45b605580d3fd49a9845d0e6f108d3068db2a6d
Kelly Woods <BYU\kwoods8@humwp-dev.byu.edu>
2022-04-11 15:26:17 -0600

    Removed post orderby lastname because it was interfering with the sort on all pages

--- functions.php
+++ functions.php
@@ -351,18 +351,6 @@ function format_phone_block ($phone) {
     return $block;
 }
 
-function posts_orderby_lastname ($orderby_statement) 
-{
-    $orderby_statement = "RIGHT(post_title, LOCATE(' ', REVERSE(post_title)) - 1) ASC";
-    return $orderby_statement;
-}
-add_filter( 'posts_orderby' , 'posts_orderby_lastname' );
-$loop = new WP_Query(
-    array (
-        'post_type' => 'person'
-    )
-);
-
 //add custom author to post of type "blog"
 function add_author_support_to_posts() {
     add_post_type_support( 'blog', 'author' ); 

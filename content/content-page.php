abe52cb8e6ae7eedac4c81c30d735c087637fe04
Kelly Woods <BYU\kwoods8@humwp-dev.byu.edu>
2022-05-17 16:23:11 -0600

    Changed h1 to h2 for pages

--- content/content-page.php
+++ content/content-page.php
@@ -16,8 +16,8 @@
 		</div>
 		<div class="article-inner">
 			<header>
-				<?php $septera_heading_tag = 'h2'; // ( is_front_page() ) ? 'h2' : 'h1';
-					the_title( '<' . $septera_heading_tag . ' class="entry-title singular-title" ' . cryout_schema_microdata( 'entry-title', 0 ) . '>', '</' . $septera_heading_tag . '>' ); ?>
+				<?php
+					the_title( '<h2 class="entry-title singular-title" ' . cryout_schema_microdata( 'entry-title', 0 ) . '>', '</h2>' ); ?>
 			   <span class="entry-meta" >
 					<?php septera_posted_edit(); ?>
 				</span>

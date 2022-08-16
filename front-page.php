d69aea40d565839495168116065a3963506d6e05
YuiZoldyck <sunny28@byu.edu>
2022-08-16 16:37:34 -0600

    created get_project_status function to return project status

--- front-page.php
+++ front-page.php
@@ -19,6 +19,7 @@ if ( 'posts' == get_option( 'show_on_front' ) ) {
 
 	get_header();
 	?>
+	<h1>status of project 28340: <?php echo get_project_status(28340);?> </h1>
 
     <div id="container" class="septera-landing-page one-column">
 		<main id="main" role="main" class="main">

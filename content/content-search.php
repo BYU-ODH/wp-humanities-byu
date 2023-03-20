e31b14c698bd68daf60f1e6640add7d22161c5de
Spencer Cheng <spencercheng2000@gmail.com>
2023-03-20 09:11:00 -0600

    Add project type

--- content/content-search.php
+++ content/content-search.php
@@ -12,11 +12,10 @@
      
     <?php break;
     case "projects":
-      project_type_display($post_id)
+      project_type_display($post_id);
     ?>
 
       <?php break;
-
     case "post":
       ?>
       <article class="searchPostSingle">
@@ -24,8 +23,9 @@
         <?php if ( has_post_thumbnail() ) {
           the_post_thumbnail('thumbnail', array('class' => 'postIMG'));
         } else { ?>
-        <img class="postIMG" src="http://localhost/odh/wp-content/uploads/humanitiesLogo.png" alt="<?php the_title(); ?>" />
-        <?php } ?>
+        <img class="postIMG" src="wp-content/uploads/humanitiesLogo.png" alt="<?php the_title(); ?>" />
+        <?php } 
+        echo( "<p>".$post_type."</p>");?>
         </picture>
         <div class="homePostText">
         <a class="pagePost" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
@@ -34,14 +34,19 @@
     </article>
     <?php break;
     default:
+    
     ?>
+    
         <article class="searchPostSingle">
         <picture class="searchImgContainer">
         <?php if ( has_post_thumbnail() ) {
           the_post_thumbnail('thumbnail', array('class' => 'postIMG'));
         } else { ?>
-        <img class="postIMG" src="http://localhost/odh/wp-content/uploads/humanitiesLogo.png" alt="<?php the_title(); ?>" />
-        <?php } ?>
+        <img class="postIMG" src="wp-content/uploads/humanitiesLogo.png" alt="<?php the_title(); ?>" />
+        <?php 
+           echo( "<p>".$post_type."</p>");
+          } 
+        ?>
         </picture>
         <div class="homePostText">
         <a class="pagePost" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>

5b5340855dff4e724b4b31afb471b22231a0b4d2
YuiZoldyck <betasan28@gmail.com>
2023-04-27 11:35:20 -0600

    closes #489 by adding in custom post search in functions

--- functions.php
+++ functions.php
@@ -477,6 +477,19 @@ function set_default_meta($post_id){
     return $post_id;
 }
 
+/**
+ * This function modifies the main WordPress query to include an array of 
+ * post types instead of the default 'post' post type.
+ *
+ * @param object $query The main WordPress query.
+ */
+function odh_include_custom_post_types_in_search_results( $query ) {
+    if ( $query->is_main_query() && $query->is_search() && ! is_admin() ) {
+        $query->set( 'post_type', array( 'post', 'collaborator', 'home_card', 'projects', 'room' ) );
+    }
+}
+add_action( 'pre_get_posts', 'odh_include_custom_post_types_in_search_results' );
+
 function admin_css() {
     wp_enqueue_style( 'admin_css', get_template_directory_uri() . '/resources/css/admin/admin-styles.css' );
 }
